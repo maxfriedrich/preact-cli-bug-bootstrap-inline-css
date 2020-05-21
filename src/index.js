5a6afa9062313d5f83ec064d9a4bb4ba4703cc1d
Max Friedrich <2mfriedr@informatik.uni-hamburg.de>
2020-05-21 11:56:33 +0200

    Add bootstrap css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import './style';
 import { Component } from 'preact';
+import 'bootstrap/dist/css/bootstrap';
 
 export default class App extends Component {
 	render() {
 		return (
-			<div>
+			<div class="container bg-success">
 				<h1>Hello, World!</h1>
 			</div>
 		);
